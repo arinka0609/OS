4a2efd2c1f109702ee279a0ca3ef5f473616a519
arinka0609 <arina.boykova.2018@gmail.com>
2022-11-03 12:34:51 +0300

    comment

--- calculate.c
+++ calculate.c
@@ -11,7 +11,7 @@
 float Calculate(float Numeral, char Operation[4])
 {
 	float SecondNumeral;
-	if(strncmp(Operation, "+", 1) == 0)
+	if(strncmp(Operation, "+", 1) == 1)
 	{
 	    printf("Второе слагаемое: "); 
 	    scanf("%f",&SecondNumeral); 
